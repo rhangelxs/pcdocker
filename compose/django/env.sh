16b37b647a2855f5e3ca4538948873cc97a4529c
rhangelxs <rhangelxs@gmail.com>
2016-03-02 21:04:15 +0300

    Pass errors

--- compose/django/env.sh
+++ compose/django/env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set +e
 # This entrypoint is used to play nicely with the current cookiecutter configuration.
 # Since docker-compose relies heavily on environment variables itself for configuration, we'd have to define multiple
 # environment variables just to support cookiecutter out of the box. That makes no sense, so this little entrypoint
