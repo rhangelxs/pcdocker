a5c180ab6dc758b5435362dbf100cc6849fb3b04
rhangelxs <rhangelxs@gmail.com>
2016-03-02 22:41:04 +0300

    Remove bash defaults

--- compose/django/env.sh
+++ compose/django/env.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set +e
 # This entrypoint is used to play nicely with the current cookiecutter configuration.
 # Since docker-compose relies heavily on environment variables itself for configuration, we'd have to define multiple
 # environment variables just to support cookiecutter out of the box. That makes no sense, so this little entrypoint
@@ -21,5 +20,5 @@ if [ -z "${GITHUB_CLONE_URL}" ]; then
 fi
 
 if [ "${INSTALL_ENTRYPOINT_PIP}" == "true" ]; then
-    pip install -r /app/pcdocker/requirements.txt
+    pip install -r /app/pcdocker/pcdocker/requirements.txt
 fi
